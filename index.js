74c162fc73290af0aa7d8dfd0635671f6e334d00
Adrian Biedrzycki <a@modules.io>
2017-05-03 08:54:24 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+const etag = require('etag');
+
+const requestTransclude = (p, fn) => new Promise((accept, reject) => {
+  fs.readFile(p, 'utf8', (err, s) => {
+    if (!err) {
+      s = fn(s);
+      const b = new Buffer(s, 'utf8');
+      b.etag = etag(b);
+
+      accept(b);
+    } else {
+      reject(err);
+    }
+  })
+});
+
+const requestTranscludeRequestHandler = (p, fn) => requestTransclude(p, fn)
+  .then(d => (req, res, next) => {
+    const ifNoneMatch = req.get('If-None-Match');
+
+    if (ifNoneMatch === d.etag) {
+      res.statusCode = 304;
+      res.end();
+    } else {
+      res.set('Etag', d.etag);
+      res.end(d);
+    }
+  });
+
+module.exports = {
+  requestTransclude,
+  requestTranscludeRequestHandler,
+};
